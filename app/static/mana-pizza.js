6781974aece4a841b03bd0d3b43e0a03e24484df
Flávio Teixeira <flavio.mtps@gmail.com>
2024-07-08 03:16:24 -0300

    Simplified mode included for app endpoints and js communications

--- app/static/mana-pizza.js
+++ app/static/mana-pizza.js
@@ -2,39 +2,85 @@ function $(id) {
     return document.getElementById(id);
 }
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+$("calculateSimpleButton").disabled = true
+$("calculateAdvancedButton").disabled = true
 
-$("calculateButton").disabled = true
-
-$("calculateButton").addEventListener("click", function() {
-    $("outputDiv").classList.add("hidden");
+$("calculateSimpleButton").addEventListener("click", function() {
+    $("outputAdvancedDiv").classList.add("hidden");
+    $("outputSimpleDiv").classList.add("hidden");
     $("errorsDiv").classList.add("hidden");
 
     var csrfToken = document.querySelector("meta[name='csrf-token']").content
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/calculate", true);
+    xhr.open("POST", "/calculate/simple", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("X-CSRFToken", csrfToken);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 response = JSON.parse(xhr.responseText)
-                $("outputLands").value = response["lands"].join("\n");
-                $("spanTotalCMC").textContent = response["cmc_total"];
-                $("spanAvgCMC").textContent = response["cmc_avg_all"];
-                $("spanAvgCMCNoLands").textContent = response["cmc_avg_wo_lands"];
-                $("spanPrice").textContent = response["total_price"];
+                $("outputSimpleLands").textContent = response["selected_lands"].join("\n");
+                $("statsSimpleLandCount").textContent = response["land_count"];
+                $("statsSimpleTotalCMC").textContent = response["cmc_total"];
+                $("statsSimpleAvgCMC").textContent = response["cmc_avg_all"];
+                $("statsSimpleAvgCMCNoLands").textContent = response["cmc_avg_wo_lands"];
                 Object.keys(response["color_proportions"]).forEach(function(k){
                     if (response["color_proportions"][k] > 0){
-                        $("spanPercent" + k).textContent = k + ": " + (response["color_proportions"][k] * 100) + "%" + "\n";
+                        $("statsSimplePercent" + k).textContent = k + ": " + (response["color_proportions"][k] * 100) + "%" + "\n";
                     }
                     else {
-                        $("spanPercent" + k).textContent = ""
+                        $("statsSimplePercent" + k).textContent = ""
                     }
                 });
-                $("outputDiv").classList.remove("hidden");
+                $("outputSimpleDiv").classList.remove("hidden");
+            } else if (xhr.status === 422) {
+                response = JSON.parse(xhr.responseText)
+                console.log(response["errors"])
+                $("spanErrors").textContent = response["errors"].join("\n");
+                $("errorsDiv").classList.remove("hidden");
+            }
+        }
+    };
+    xhr.send(
+        JSON.stringify({
+            commander: $("inputCommander").value,
+            cards: $("inputCards").value.split("\n")
+        })
+    );
+});
+
+$("calculateAdvancedButton").addEventListener("click", function() {
+    $("outputAdvancedDiv").classList.add("hidden");
+    $("outputSimpleDiv").classList.add("hidden");
+    $("errorsDiv").classList.add("hidden");
+
+    var csrfToken = document.querySelector("meta[name='csrf-token']").content
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/calculate/advanced", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-CSRFToken", csrfToken);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                response = JSON.parse(xhr.responseText)
+                $("outputAdvancedLands").value = response["selected_lands"].join("\n");
+                $("statsAdvancedLandCount").textContent = response["land_count"];
+                $("statsAdvancedTotalCMC").textContent = response["cmc_total"];
+                $("statsAdvancedAvgCMC").textContent = response["cmc_avg_all"];
+                $("statsAdvancedAvgCMCNoLands").textContent = response["cmc_avg_wo_lands"];
+                $("statsAdvancedLandbasePrice").textContent = response["landbase_price"];
+                $("statsAdvancedDeckPrice").textContent = response["deck_price"];
+                Object.keys(response["color_proportions"]).forEach(function(k){
+                    if (response["color_proportions"][k] > 0){
+                        $("statsAdvancedPercent" + k).textContent = k + ": " + (response["color_proportions"][k] * 100) + "%" + "\n";
+                    }
+                    else {
+                        $("statsAdvancedPercent" + k).textContent = ""
+                    }
+                });
+                $("outputAdvancedDiv").classList.remove("hidden");
             } else if (xhr.status === 422) {
                 response = JSON.parse(xhr.responseText)
                 console.log(response["errors"])
@@ -54,29 +100,51 @@ $("calculateButton").addEventListener("click", function() {
     );
 });
 
-$("clearButton").addEventListener("click", function() {
-    $("outputDiv").classList.add("hidden");
+$("closeErrorsDiv").addEventListener("click", function() {
     $("errorsDiv").classList.add("hidden");
+});
+
+$("closeSimpleOutputDiv").addEventListener("click", function() {
+    $("outputSimpleDiv").classList.add("hidden");
+});
+
+$("closeAdvancedOutputDiv").addEventListener("click", function() {
+    $("outputAdvancedDiv").classList.add("hidden");
+});
+
+$("clearSimpleInputsButton").addEventListener("click", function() {
+    $("inputCards").value = "";
+    $("inputCommander").value = "";
+});
+
+$("clearAdvancedInputsButton").addEventListener("click", function() {
+    $("inputCards").value = "";
+    $("inputCommander").value = "";
+    $("inputPriceLimit").value = "10";
 });
 
 $("inputCards").addEventListener("input", function() {
     if ($("inputCards").value == "" || $("inputCommander").value == "") {
-        $("calculateButton").disabled = true
+        $("calculateSimpleButton").disabled = true
+        $("calculateAdvancedButton").disabled = true
     } else {
-        $("calculateButton").disabled = false
+        $("calculateSimpleButton").disabled = false
+        $("calculateAdvancedButton").disabled = false
     }
 });
 
 $("inputCommander").addEventListener("input", function() {
     if ($("inputCards").value == "" || $("inputCommander").value == "") {
-        $("calculateButton").disabled = true
+        $("calculateSimpleButton").disabled = true
+        $("calculateAdvancedButton").disabled = true
     } else {
-        $("calculateButton").disabled = false
+        $("calculateSimpleButton").disabled = false
+        $("calculateAdvancedButton").disabled = false
     }
 });
 
 $('copyResult').addEventListener('click', function() {
-    var text = $('outputLands');
+    var text = $('outputAdvancedLands');
     text.select();
     text.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(text.value);
